de50e2de9e7347c4989534b1776ae1bb7c2251b5
ivan soto <ivansotocaro@gmail.com>
2025-01-21 13:42:38 -0500

    feat: agregando formulario de registro con todos los campos

---
+++ src/app/register/register.page.ts
@@ -0,0 +1,73 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.page.html',
+  styleUrls: ['./register.page.scss'],
+  standalone: false,
+})
+export class RegisterPage implements OnInit {
+
+  registerForm: FormGroup
+  errorMessage: any;
+
+  formErrors = {
+    user: [
+      { type: 'required', message: 'El usuario es obligatorio' },
+    ],
+    name: [
+      { type: 'required', message: 'El nombre es obligatorio' },
+    ],
+    lastname: [
+      { type: 'required', message: 'El apellido es obligatorio' },
+    ],
+    email: [
+      { type: 'required', message: 'El correo es obligatorio' },
+      { type: 'email', message: 'El correo no es valido' },
+    ],
+    password: [
+      { type: 'required', message: 'La contraseña es obligatoria' },
+      {
+        type: 'minlength',
+        message: 'La contraseña debe tener al menos 6 caracteres',
+      },
+    ],
+    passwordConfirmation: [
+      { type: 'required', message: 'La confirmación de la contraseña es obligatoria' },
+      {
+        type: 'minlength',
+        message: 'La confirmación de la contraseña debe tener al menos 6 caracteres',
+      },
+    ],
+  };
+
+  constructor(private formBuilder: FormBuilder) {
+
+    this.registerForm = this.formBuilder.group({
+      user: ['', [Validators.required]],
+      name: ['', [Validators.required]],
+      lastname: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      passwordConfirmation: [
+        '',
+        [Validators.required, Validators.minLength(6)],
+      ],
+    });
+
+  }
+
+  ngOnInit() {}
+
+  registerUser(credentials: any) {
+    console.log(credentials);
+  }
+
+}
